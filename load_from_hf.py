0e1980142679b69ad2984fb0acc8db76df6aceca
HieuMan <hieuman2708@gmail.com>
2022-05-13 09:45:56 +0700

    change: save path

--- load_from_hf.py
+++ load_from_hf.py
@@ -12,7 +12,7 @@ def download_to_local(repo_id: str, save_dir: str):
 
 if __name__ == '__main__':
     pretrains = ['roberta-base', 'bert-base-multilingual-uncased', 'roberta-large', 'xlm-roberta-base']
-    save_dir = '~/hf_hub/'
+    save_dir = '/hf_hub'
     for model in pretrains:
         new_model_dir = download_to_local(model, save_dir)
         print(f"Loaded and saved {model} in {new_model_dir}")
